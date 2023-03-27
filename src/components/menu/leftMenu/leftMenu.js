0dbefb2dc63bad7a9afe3d7523dd50867617e4ba
Ricardo Beaujon <rbeaujon77@gmail.com>
2023-03-27 11:34:14 +0300

    refactor: changed the main page dashboard to games

--- src/components/menu/leftMenu/leftMenu.js
+++ src/components/menu/leftMenu/leftMenu.js
@@ -39,9 +39,9 @@ export const LeftMenu = () => {
         > X </button> 
 
         <div className={'icons'}>
-          <div onClick={() => { navigate("/"); setActiveMenu(false); }}><img src={dashboard} alt="dashboard" /><label>Dashboard</label></div>
+          <div onClick={() => { navigate("/dashboard"); setActiveMenu(false); }}><img src={dashboard} alt="dashboard" /><label>Dashboard</label></div>
           <div onClick={() => { navigate("/users"); setActiveMenu(false); }}><img src={users} alt="users" /><label>Users</label></div>
-          <div onClick={() => { navigate("/games"); setActiveMenu(false); }}><img src={games} alt="games" /><label>Games</label></div>
+          <div onClick={() => { navigate("/"); setActiveMenu(false); }}><img src={games} alt="games" /><label>Games</label></div>
           <div onClick={() => { navigate("/analytics"); setActiveMenu(false); }}><img src={analytics} alt="analytics" /><label>Analytics</label></div>
           <div onClick={() => { navigate("/messages"); setActiveMenu(false); }}><img src={messages} alt="messages" /><label>Messages</label></div>
           <div onClick={() => { navigate("/calendar"); setActiveMenu(false); }}><img src={calendar} alt="calendar" /><label>Calendar</label></div>
